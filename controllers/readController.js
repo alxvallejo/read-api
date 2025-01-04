adcb91cc3d597c3fc196940d9b40518288b9a5c9
alexatbowst <alex@bowst.com>
2025-01-04 12:33:53 -0500

    Small update to set the user-agent

--- controllers/readController.js
+++ controllers/readController.js
@@ -1,34 +1,39 @@
 var read = require('node-readability');
 
-
 module.exports = {
-    readUrl: (url) => {
-        return new Promise((resolve, reject) => {
-            let httpsUrl = url.replace("http://", "https://");
-            try {
-                read(httpsUrl, (err, article, meta) => {
-                    if (!article) {
-                        reject(null)
-                    }
-                    // debugger;
-                    //console.log(meta)
-                    let content = article ? article.content : null;
-                    let title = article ? article.title : null;
-
-                    if (!article) {
-                        resolve(null)
-                    } else {
-                        article.close()
-                        resolve({ content, title })
-                    }
-
-
-                })
+  readUrl: (url) => {
+    return new Promise((resolve, reject) => {
+      let httpsUrl = url.replace('http://', 'https://');
+      try {
+        read(
+          httpsUrl,
+          {
+            headers: {
+              'User-Agent': 'web:socket:v1.2.0 (by /u/no_spoon)',
+            },
+          },
+          (err, article, meta) => {
+            if (!article) {
+              reject(null);
             }
-            catch (error) {
-                reject(error)
-            }
-        })
-    }
-}
+            // debugger;
+            //console.log(meta)
+            console.log('article: ', article);
+            let content = article ? article.content : null;
 
+            let title = article ? article.title : null;
+
+            if (!article) {
+              resolve(null);
+            } else {
+              article.close();
+              resolve({ content, title });
+            }
+          }
+        );
+      } catch (error) {
+        reject(error);
+      }
+    });
+  },
+};
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const read = require('./controllers/readController.js');
-var cors = require('cors')
-var bodyParser = require('body-parser')
-var path = require('path')
-var fs = require('fs')
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var path = require('path');
+var fs = require('fs');
 const express = require('express');
-var helmet = require('helmet')
-var https = require('https')
+var helmet = require('helmet');
+var https = require('https');
 const app = express();
 const port = 3000;
 
@@ -14,39 +14,39 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
 // var certOptions = {
 //     key: fs.readFileSync(path.resolve('./config/cert/key.pem')),
 //     cert: fs.readFileSync(path.resolve('./config/cert/cert.pem'))
 // }
 
 app.get('/', (req, res) => {
-    res.send('An alligator approaches!');
+  res.send('An alligator approaches!');
 });
 
 app.post('/getContent', (req, res) => {
-    //console.log('req.body', req.body)
-    //res.send('Some data')
+  //console.log('req.body', req.body)
+  //res.send('Some data')
 
-    read.readUrl(req.body.url)
-        .then((content) => {
+  read.readUrl(req.body.url).then(
+    (content) => {
+      res.send(content);
+    },
+    (err) => {
+      res.send(err);
+    }
+  );
 
-            res.send(content)
-        }, (err) => {
-            res.send(err)
-        })
-
-    // try {
-    //     let content = read.readUrl(req.body.url)
-    //         .then(())
-    //     console.log('content', content)
-    //     res.send(content)
-    // }
-    // catch (err) {
-    //     console.log(err)
-    //     res.send(err)
-    // }
-})
+  // try {
+  //     let content = read.readUrl(req.body.url)
+  //         .then(())
+  //     console.log('content', content)
+  //     res.send(content)
+  // }
+  // catch (err) {
+  //     console.log(err)
+  //     res.send(err)
+  // }
+});
 
 //var server = https.createServer(certOptions, app).listen(port, () => console.log('Alex made a thing at port ' + port))
 
